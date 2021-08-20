64ca4c0b8fd6703131c387b4bbd492deb66de8b0
Léo Igor Nunes de Oliveira <64155993+c4p1t4n@users.noreply.github.com>
2021-08-20 13:31:12 -0300

    Add files via upload

---
+++ NumerosPares.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bandtec.projetodoisnivelamento;
+
+/**
+ *
+ * @author Aluno
+ */
+public class NumerosPares {
+    public static void main(String[] args) {
+     
+     int cont=0;
+     while(cont <=40){
+         
+        if (cont % 2 ==0){
+            System.out.println(cont);
+        }
+        cont++;
+        
+        }
+    }
+}
