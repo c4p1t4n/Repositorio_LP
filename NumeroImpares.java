fb99da128fbfda1d1d9afe2020c7e5da5bfc28b8
Léo Igor Nunes de Oliveira <64155993+c4p1t4n@users.noreply.github.com>
2021-08-20 13:24:36 -0300

    Add files via upload

---
+++ NumeroImpares.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bandtec.projetodoisnivelamento;
+
+/**
+ *
+ * @author Aluno
+ */
+public class NumeroImpares {
+    public static void main(String[] args) {
+        for(int cont=0;cont < 91;cont++){
+        if (cont % 2 !=0){
+            System.out.println(cont);
+        }
+        
+        
+        }
+    }
+    
+}
